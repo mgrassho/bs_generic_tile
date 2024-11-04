0f93ddb39103b5872081204c1ffc8d6ca97daccd
Martin Grasshoff <martin.grasshoff@sap.com>
2024-11-04 15:31:58 +0100

    logistics work

--- src/Assets.ts
+++ src/Assets.ts
@@ -1,5 +1,5 @@
 import "@ui5/webcomponents-theming/dist/Assets.js"; // Theming
 
-// own mg_webc_generic_tile package assets
+// own bs_webc_generic_tile package assets
 import "./generated/json-imports/Themes.js";
 import "./generated/json-imports/i18n.js";

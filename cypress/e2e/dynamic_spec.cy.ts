3cf672198ef6b48ed52be035f0091b43c83f520e
Martin Grasshoff <martin.grasshoff@sap.com>
2024-11-11 14:24:51 +0100

    added more test
    button click tests

--- cypress/e2e/dynamic_spec.cy.ts
+++ cypress/e2e/dynamic_spec.cy.ts
@@ -5,15 +5,24 @@ describe('tile events should be tested', () => {
     cy.get('[id="myFirstComponent"]').should('be.visible').as('tile')
     
     cy.get('@tile').then(tile => {
-        tile.get(0).addEventListener("click", cy.stub().as('clicked'));
+        tile.get(0).addEventListener("click", cy.stub().as('clicked'))
     });
 
 
     cy.get('@tile')
-			.click();
+			.click()
 
     cy.get("@clicked")
-			.should("have.been.calledOnce");  
+			.should("have.been.calledOnce")
 
+    
+    //Need to implement focusable
+    cy.get('@tile')
+      .realPress('Space')
+
+    //cy.get("@clicked")
+		//	.should("have.been.calledTwice")
+
+    
   })
 })
